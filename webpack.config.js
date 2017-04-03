153375f014ba6ade14fa67f2b9114863a8d8bb23
Lei Pi <lpi@memphis.edu>
2017-04-03 14:45:51 -0500

    test npm config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     entry: "./src/index.ts",
     output: {
-        filename: "bundle.js",
+        filename: "index.js",
         path: __dirname + "/dist"
     },
     resolve: {
